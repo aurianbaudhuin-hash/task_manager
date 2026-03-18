daf8dd0be427dc730539cc40f9999353382480b0
Aurian Baudhuin <aurian.baudhuin@gmail.com>
2026-03-18 14:27:08 +0100

    Initial commit, basic project structure

---
+++ tasks/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ("not_started", "Not started"),
+        ("in_progress", "In progress"),
+        ("done", "Done"),
+    ]
+    PRIORITY_CHOICES = [
+    (1, "High"),
+    (2, "Medium"),
+    (3, "Low"),
+    ]
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    due_date = models.DateField(null=True, blank=True)
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="not_started"
+    )
+    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=2)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
