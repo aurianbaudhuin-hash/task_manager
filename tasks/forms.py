4f5082db1148bbb81bdd268e2ad2ff9316d29dd6
Aurian Baudhuin <aurian.baudhuin@gmail.com>
2026-03-19 14:53:31 +0100

    Bootstrapped all pages to improve appearance

--- tasks/forms.py
+++ tasks/forms.py
@@ -2,11 +2,29 @@ from django import forms
 from .models import Task
 
 class TaskForm(forms.ModelForm):
-    due_date = forms.DateField(
-        required=False,
-        widget=forms.DateInput(attrs={"type": "date"})
-    )
-
     class Meta:
         model = Task
-        fields = ["title", "description", "owner", "due_date", "status", "comment"]
+        fields = ['title', 'description', 'due_date', 'status', 'comment', 'owner']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': ' '
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': ' ',
+                'style': 'height: 100px'
+            }),
+            'due_date': forms.DateInput(attrs={
+                'class': 'form-control',
+                'type': 'date'
+            }, format='%Y-%m-%d'),
+            'status': forms.Select(attrs={
+                'class': 'form-select'
+            }),
+            'comment': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': ' ',
+                'style': 'height: 100px'
+            }),
+        }
